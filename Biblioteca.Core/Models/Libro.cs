0129e073aa9ff76e84139b91589c19bbf920cb2f
Tommaso <t.paparesta@gmail.com>
2025-07-26 11:45:19 +0200

    Iniziato struttura progetto Biblioteca

---
+++ Biblioteca.Core/Models/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace Biblioteca.Core.Models;
+
+public class Libro
+{
+    public int IdLibro { get; set; }
+    public string Titolo { get; set; }
+    public int IdAutore { get; set; }
+    public int Anno { get; set; }
+    public int IdPaese { get; set; }
+    public int IdLingua { get; set; }
+    public string CostoLibro { get; set; }
+    public int Pagine { get; set; }
+}
---
+++ Biblioteca.Core/Models/Utenti.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblioteca.Core.Models;
+
+public class Utenti
+{
+    public int IdUtente { get; set; }
+    public string DataNascita { get; set; }
+    public int Nome { get; set; }
+    public int Cognome { get; set; }
+    public int Email { get; set; }
+
+}
